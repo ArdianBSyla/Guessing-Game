0e6a3d34fcfaa9b9f2a109db9936531a9f5980b7
unknown <ardian.syla@studentet.uni-pr.edu>
2019-08-28 16:44:46 +0200

    adde last line to be printed at the end of the game

--- GameController.java
+++ GameController.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class GameController {
     public static void main(String[] args) {
-    	//Scanner reader = new Scanner(System.in);
+    	Scanner reader = new Scanner(System.in);
         GuessingGame game = new GuessingGame();
         game.play(1,1000);
+	System.out.println("Thank You!!!");
     }    
 }
